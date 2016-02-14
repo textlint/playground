977f67c10a9dfe0797c6c7f8b8906b4c3cb0a937
azu <azuciao@gmail.com>
2016-02-15 00:16:56 +0900

    chore(footer): add site's

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export const Footer = {
         return <div className="Footer">
             <ul className="Footer-list">
                 <li className="Footer-list-item">
-                    The source code is available on <a href="https://github.com/textlint/textlint.github.io">GitHub</a>
+                    The site's source code is available on <a href="https://github.com/textlint/textlint.github.io">GitHub</a>
                 </li>
                 <li className="Footer-list-item">
                     Copyright © 2014- <a href="https://github.com/azu">@azu</a> All rights reserved.
