fe9ded2b940f28c7024e3e0d3ac48eb32bd93fad
azu <azuciao@gmail.com>
2016-02-15 09:35:44 +0900

    feat(component): add npm link

--- src/components/TextlintDemo.js
+++ src/components/TextlintDemo.js
@@ -12,7 +12,8 @@ export const TextlintDemo = {
         return <div className="TextlintDemo">
             <h2 className="TextlintDemo-title header-has-icon">Online DEMO</h2>
             <p className="TextlintDemo-lead">
-                Take textlint for a spin, start typing below. Want to use more? Go to <a href="https://github.com/textlint/textlint" title="textlint/textlint">GitHub</a>
+                Take textlint for a spin, start typing below. Want to use more? Go to <a
+                href="https://github.com/textlint/textlint" title="textlint/textlint">GitHub</a>
             </p>
             <div className="TextlintDemo-Content l-flex-container l-flex-row">
                 <div className="TextlintDemo-Editor l-flex-left-half">
@@ -20,16 +21,17 @@ export const TextlintDemo = {
                 </div>
                 <div className="TextlintDemo-Setting l-flex-right-half l-flex-container l-flex-col">
                     <div className="TextlintDemo-ruleBox l-flex-top-half">
+                        <h3>Rule Errors</h3>
+                        <TextlintErrorList errors={ruleErrors}/>
+                    </div>
+                    <div className="TextlintDemo-errorBox l-flex-bottom-half">
                         <h3>Rule Lists</h3>
                         <div className="TextlintRuleListSet">
                             <EnabledTextlintRuleList rules={enabledRules}/>
                             <DisabledTextlintRuleList rules={disabledRules}/>
                         </div>
                         <AddTextlintRule />
-                    </div>
-                    <div className="TextlintDemo-errorBox l-flex-bottom-half">
-                        <h3>Rule Errors</h3>
-                        <TextlintErrorList errors={ruleErrors}/>
+
                     </div>
                 </div>
             </div>
--- src/components/TextlintDemo/TextlintErrorList.js
+++ src/components/TextlintDemo/TextlintErrorList.js
@@ -6,12 +6,19 @@ export default {
     render({props}){
         const errorList = props.errors.map(error => {
             const showMessage = (message) => {
-                return message.split(/\n/).map(line => {
-                    return <span>{line}<br/></span>;
+                var lines = message.split(/\n/);
+                return lines.map((line, index) => {
+                    if (index < (lines.length - 1)) {
+                        return <span>{line}<br /></span>;
+                    }
+                    return <span>{line}</span>;
                 });
             };
+            const npmLink = (ruleId) => {
+                return <a href={"https://www.npmjs.com/package/" + ruleId}>{ruleId}</a>;
+            };
             return <li key={`${error.id}${error.line}${error.column}`}>
-                <p>{error.line}:{error.column} {showMessage(error.message)} ({error.ruleId})</p>
+                <p>{error.line}:{error.column} {showMessage(error.message)} ({npmLink(error.ruleId)})</p>
             </li>
         });
         return <div class="TextlintErrorList">
