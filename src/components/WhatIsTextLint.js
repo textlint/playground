90731f3a66b6c6335b40b1176bc05278a4ab0cc0
Lucas Bento <lucas.bsilva@outlook.com>
2016-02-17 17:46:23 -0200

    Corrected a few typos

--- src/components/WhatIsTextLint.js
+++ src/components/WhatIsTextLint.js
@@ -7,22 +7,22 @@ export const WhatIsTextLint = {
             <h2 className="WhatIsTextLint-title header-has-icon">What is textlint?</h2>
             <p>
                 textlint is an open source text linting utility written in JavaScript.<br />
-                It it hard to lint natural language texts, but we try to resolve this issue by <b>pluggable</b> approach.
+                It is hard to lint natural language texts, but we try to resolve this issue by <b>pluggable</b> approach.
             </p>
             <h3 className="WhatIsTextLint-title">Everything is pluggable:</h3>
             <ul className="WhatIsTextLint-philosophy-list">
                 <li>No bundle rules.</li>
-                <li>To use rule, run simply <code>npm install textlint-rule-xxx</code>. See a <a
+                <li>To use a rule, run simply <code>npm install textlint-rule-xxx</code>. See a <a
                     href="https://github.com/textlint/textlint/wiki/Collection-of-textlint-rule"
-                    title="Collection of textlint rule · textlint/textlint Wiki">collection of textlint rule</a>
+                    title="Collection of textlint rule · textlint/textlint Wiki">collection of textlint rules</a>
                 </li>
                 <li><a href="https://github.com/textlint/textlint-plugin-markdown"
                        title="Markdown support for textlint.">Markdown</a> and <a
                     href="https://github.com/textlint/textlint-plugin-text" title="plain txt support for textlint">plain
-                    text</a> are support by default.
+                    text</a> are supported by default.
                     Additionally, <a href="https://github.com/textlint/textlint-plugin-html"
                                      title="textlint/textlint-plugin-html: html support for textlint">HTML</a> and other
-                    format support by custom plugin
+                    formats are supported by custom plugins
                 </li>
                 <li>Formatter(reporter) is used both by bundled and custom formatters</li>
             </ul>
