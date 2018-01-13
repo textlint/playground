bf4394b4369daaa80f631f6db919b82e939c6b3c
azu <azuciao@gmail.com>
2018-01-13 23:53:39 +0900

    test(cypress): integration with power-assert

--- cypress/integration/index-spec.js
+++ cypress/integration/index-spec.js
@@ -1,18 +1,20 @@
 // MIT © 2018 azu
 "use strict";
+const assert = require("assert");
+
 const DefaultErrorList = 4;
 describe("The Home Page", function() {
     it("successfully loads", function() {
         cy.visit("/");
 
         cy.get(".TextlintErrorList-listItem").should(e => {
-            assert.isAbove(e.length, DefaultErrorList);
+            assert.ok(e.length === 4);
         });
     });
     it("Delete all text and disappear error list items", function() {
         cy.visit("/");
         cy.get(".TextlintErrorList-listItem").should(e => {
-            assert.isAbove(e.length, DefaultErrorList);
+            assert.ok(e.length === DefaultErrorList);
         });
 
         cy
