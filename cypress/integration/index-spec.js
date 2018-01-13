99b15744e225b2a2ef3111f216baa21566aac80d
azu <azuciao@gmail.com>
2018-01-14 00:31:33 +0900

    test(cypress): add toggle feature

--- cypress/integration/index-spec.js
+++ cypress/integration/index-spec.js
@@ -4,19 +4,43 @@ const assert = require("assert");
 
 const DefaultErrorList = 4;
 describe("The Home Page", function() {
-    it("successfully loads", function() {
+    beforeEach(function() {
         cy.visit("/");
-
         cy.get(".TextlintErrorList-listItem").should(e => {
             assert.ok(e.length > DefaultErrorList);
         });
     });
-    it("Delete all text and disappear error list items", function() {
-        cy.visit("/");
+
+    it("successfully loads", function() {
         cy.get(".TextlintErrorList-listItem").should(e => {
             assert.ok(e.length > DefaultErrorList);
         });
+    });
 
+    it("should toggle rule", function() {
+        cy.get(".DisabledTextlintRuleList .TextLintRuleListItem").should(e => {
+            assert.ok(e.length === 0);
+        });
+        cy
+            .get(".EnabledTextlintRuleList .TextLintRuleListItem")
+            .first()
+            .click();
+        // test disable
+        cy.get(".DisabledTextlintRuleList .TextLintRuleListItem").should(e => {
+            assert.ok(e.length === 1);
+        });
+        // click disable
+        cy
+            .get(".DisabledTextlintRuleList .TextLintRuleListItem")
+            .first()
+            .click();
+        // enable again
+        cy.get(".DisabledTextlintRuleList .TextLintRuleListItem").should(e => {
+            assert.ok(e.length === 0);
+        });
+    });
+
+    it("Delete all text and disappear error list items", function() {
         // {selectall}{backspace} workaround for CodeMirror
         cy
             .get(".CodeMirror textarea")
